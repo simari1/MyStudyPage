07b09b8694107eb5428e0b7873cbd1ca1e7a0047
Arito Shimazaki <bdfsg610@yahoo.co.jp>
2021-07-17 17:31:24 +0900

    add 20

---
+++ View/Learn/JavaScript30/20 - Speech Detection/scripts.js
@@ -0,0 +1,28 @@
+$(document).ready(function () {
+  window.SpeechRecognition =
+    window.SpeechRecognition || window.webkitSpeechRecognition;
+
+  const recognition = new SpeechRecognition();
+  recognition.lang = "en-us";
+  // recognition.interimResults = true;
+
+  let p = $("<p editable>");
+  let words = $(".words");
+
+  recognition.addEventListener("result", e => {
+    console.log(e.results);
+    let transcript = [...e.results]
+    .map(r => r[0].transcript)
+    .join(" ");
+
+    p.text(transcript);
+
+    if (e.results[0].isFinal) {
+      p = $("<p>");
+      words.append(p);
+    }
+  });
+
+  recognition.addEventListener("end", recognition.start);
+  recognition.start();
+});
