63e37ab977945ba9fabafd4ed65efefcb6be590a
Arito Shimazaki <bdfsg610@yahoo.co.jp>
2021-08-08 19:40:48 +0900

    remove settimeout

--- api/GetMessage/index.js
+++ api/GetMessage/index.js
@@ -1,20 +1,18 @@
 module.exports = async function (context, req) {
-    context.log('JavaScript HTTP trigger function processed a request.');
+  context.log("JavaScript HTTP trigger function processed a request.");
 
-    setTimeout(function(){
-        context.res = {
-            body: { text: "Hello from the Azure Functions API" }
-        };
-    }, 3000);
-    
-    context.log('JavaScript HTTP trigger function ended');
-    // const name = (req.query.name || (req.body && req.body.name));
-    // const responseMessage = name
-    //     ? "Hello, " + name + ". This HTTP triggered function executed successfully."
-    //     : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
+  context.res = {
+    body: { text: "Hello from the Azure Functions API" },
+  };
 
-    // context.res = {
-    //     // status: 200, /* Defaults to 200 */
-    //     body: responseMessage
-    // };
-}
+  context.log("JavaScript HTTP trigger function ended");
+  // const name = (req.query.name || (req.body && req.body.name));
+  // const responseMessage = name
+  //     ? "Hello, " + name + ". This HTTP triggered function executed successfully."
+  //     : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
+
+  // context.res = {
+  //     // status: 200, /* Defaults to 200 */
+  //     body: responseMessage
+  // };
+};
