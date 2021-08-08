097663174a0dde7b2be7eba884f6672ebb1c9964
Arito Shimazaki <bdfsg610@yahoo.co.jp>
2021-08-08 18:16:20 +0900

    Revert "add Azure function api"
    This reverts commit db6cf6cd990007b26988e73c588bb244bcd26474.

--- View/Learn/02_Ajax/Ajax.js
+++ View/Learn/02_Ajax/Ajax.js
@@ -1,40 +1,26 @@
-$("#sample1").on("click", function () {
+$("#sample1").on("click", function(){
   $.ajax({
-    url: "text1.txt",
+    url:"text1.txt",
     type: "GET",
-    dataType: "text",
-    success: function (data) {
+    dataType:"text",
+    success: function(data){
       $("#result_sample1").text(data);
-    },
-  });
+    }
+  })
 });
 
-$("#sample2").on("click", function () {
+$("#sample2").on("click", function(){
   let baseApi = "https://api.openweathermap.org/data/2.5/weather";
-  let appId = "85f8f77814397193df652bb495ed6214";
+  let appId = "85f8f77814397193df652bb495ed6214"
   let city = $("#city").val();
   let urlWeather = baseApi + "?appid=" + appId + "&q=" + city + "&units=metric";
-
-  $.getJSON(urlWeather, function (data) {
-    $("#result_sample2").html(
-      "現在の天気：" +
-        data.weather[0].main +
-        "<br>" +
-        "現在の気温：" +
-        data.main.temp
-    );
-  });
-});
-
-$("sample3").on("click", function () {
-  let url = "/api/message";
-  $.ajax({
-    url: url,
-    type: "get",
-    dataType: "json",
-  }).then(function (response) {
-    if (response.success) {
-      $("result_sample3").val(response.getJSON().text);
+  
+  $.getJSON(
+    urlWeather,
+    function(data){
+      $("#result_sample2").html(
+        "現在の天気：" + data.weather[0].main + "<br>" +
+        "現在の気温：" + data.main.temp);
     }
-  });
+  )
 });
